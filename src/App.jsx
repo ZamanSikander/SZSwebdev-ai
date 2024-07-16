136e53465a6af0afc5804d0508c3ac5df5308b3c
Zaman Sikander <sheikhzaman857@gamil.com>
2024-07-16 22:25:49 +0500

    updated the UI and color scheme of layout. also added header and footer section

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,10 @@ function App() {
   // eslint-disable-next-line no-unused-vars
   const [fullAnswer, setFullAnswer] = useState("");
   const [displayedAnswer, setDisplayedAnswer] = useState("");
-  const [loading, setLoading] = useState(false);
 
   const apikey = import.meta.env.VITE_API_KEY;
 
   async function generateAnswers() {
-    setLoading(true);
     setFullAnswer("");
     setDisplayedAnswer("Generating Answer....");
 
@@ -30,7 +28,6 @@ function App() {
     } catch (error) {
       console.error("Error fetching answer:", error);
       setDisplayedAnswer("Error fetching answer. Please try again.");
-      setLoading(false);
     }
   }
 
@@ -46,7 +43,6 @@ function App() {
         currentIndex++;
       } else {
         clearInterval(intervalId);
-        setLoading(false);
       }
     }, 50); // Adjust the interval time (in milliseconds) as needed
   };
@@ -62,12 +58,15 @@ function App() {
   };
 
   return (
-    <div className="bg-gray-100 flex items-center justify-center min-h-screen p-4 border-2">
-      <div className=" w-1/2 text-2xl sm:text-2xl text-center mb-4 sm:mb-6 flex items-center justify-center flex-col">
-        <h1 className="m-4">SZS WEB DEV- AI</h1>
+    <div className="bg-gray-800 flex items-center justify-center min-h-screen p-4">
+      <div className=" sm:w-1/2  text-2xl  sm:text-2xl text-center mb-4 sm:mb-6 flex items-center justify-center flex-col">
+        <div>
+          <h1 className="text-gray-200 mb-8">Powered By <a href="https://szswebdev.netlify.app" className="underline" target="_blank">SZSwebdev</a></h1>
+        </div>
+        <h1 className="m-4 text-white text-lg">SZS WEB DEV- AI</h1>
 
         <textarea
-          className="w-full p-2 sm:p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 mb-4 text-base"
+          className="bg-gray-700 text-gray-200 w-full p-2 sm:p-4 border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 mb-4 text-base"
           value={question}
           onChange={(e) => setQuestion(e.target.value)}
           cols={30}
@@ -78,17 +77,17 @@ function App() {
         <button
           type="button"
           onClick={generateAnswers}
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white text-sm py-2 sm:py-3 px-4 rounded-lg mb-4 transition duration-300 ease-in-out transform hover:scale-105"
-          disabled={loading}
+          className="w-full bg-blue-800 hover:bg-blue-700 text-white text-sm py-2 sm:py-3 px-4 rounded-lg mb-4 transition duration-300 ease-in-out transform hover:scale-95"
+
         >
-          {loading ? "Generating..." : "Generate Answer"}
+          Generate Answer
         </button>
 
         <div
-          className="w-full p-2 sm:p-4 bg-gray-200 rounded-lg overflow-x-auto text-base leading-8"
+          className=" bg-gray-700 text-gray-200 w-full p-2 sm:p-4 rounded-lg overflow-x-auto text-base leading-8"
           dangerouslySetInnerHTML={createMarkup(displayedAnswer)}
         ></div>
-        <p className="text-xs bg-neutral-500 text-white fixed bottom-0 p-2">SZS WEB DEV -AI can make mistakes. Check the information before use!</p>
+        <p className="text-xs bg-gray-600 text-gray-200  fixed bottom-0 p-1 rounded sm:p-2">SZS WEB DEV -AI can make mistakes. Check the information before use!</p>
       </div>
     </div>
   );
